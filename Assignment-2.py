b27988c84f0a9adae5f2d85aa2575d80e09de90e
utk1106 <thaokarutkarsh@gmail.com>
2022-01-30 17:21:07 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Send the Words To Mirror Dimension
+
+# In[6]:
+
+
+a = input("Input a word to reverse: ")
+
+for char in range(len(a) - 1, -1, -1):
+    print(a[char], end="")
+print("\n")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
